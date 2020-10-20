2bab0d4f369797db89b7f078fa560b538868e30e
Luis <luisglopez7777@gmail.com>
2020-10-19 20:31:05 -0500

    Modulo Reach Router terminado

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,34 +1,43 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { ImgWrapper, Button, Img, Article } from './styles'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { ImgWrapper, Img, Article } from './styles'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
+import { FavButton } from '../FavButton'
+import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 
 const DEPHAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-
 
 export const PhotoCard = ({ id, likes = 0, src = DEPHAULT_IMAGE }) => {
     const [show, element] = useNearScreen()
     const key = `like-${id}`
     const [liked, setLiked] = useLocalStorage(key, false)
 
-    console.log(liked)
-
-    const Icon = liked ? MdFavorite : MdFavoriteBorder
 
     return (
         <Article ref={element}>
             {
                 show && <>
-                    <a href={`/detail/${id}`}>
+                    <a href={`/?detail=${id}`}>
                         <ImgWrapper>
                             <Img src={src} alt="" />
                         </ImgWrapper>
                     </a>
 
-                    <Button onClick={() => setLiked(!liked)}>
-                        <Icon size='32px' /> {likes} likes!
-                    </Button>
+                    <ToggleLikeMutation>
+                        {
+                            (toggleLike) => {
+                                const handleFavClick = () => {
+                                    !liked && toggleLike({
+                                        variables: {
+                                            input: { id }
+                                        }
+                                    })
+                                    setLiked(!liked)
+                                }
+                                return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
+                            }
+                        }
+                    </ToggleLikeMutation>
                 </>
             }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDom from "react-dom";
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 import { App } from './App.js'
 
-ReactDom.render(<App />, document.getElementById("app"));
+const client = new ApolloClient({
+    uri: 'https://petgram-server-luisglopez7777.vercel.app/graphql'
+})
+
+ReactDom.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById("app"));
+
