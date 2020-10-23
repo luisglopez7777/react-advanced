67e64f4530f034e985dfdb79fda449b6abae9721
Luis <luisglopez7777@gmail.com>
2020-10-22 20:12:10 -0500

    Curso terminado

---
+++ cypress/integration/petgram/test_spec.js
@@ -0,0 +1,21 @@
+/* global describe, it, cy */
+
+describe('Petgram', function () {
+    it('Para ver si la app funciona', function () {
+        cy.visit('/')
+    })
+    it('Navegamos a una categoría y vemos fotos', function () {
+        cy.visit('/pet/1')
+        cy.get('article')
+    })
+    it('Si podemos navegar con la navbar a la home', function () {
+        cy.visit('/pet/1')
+        cy.get('nav a').first().click()
+        cy.url().should('include', '/')
+    })
+    it('Los usuarios no registrados ven el formulario de registro al ir a favs', function () {
+        cy.visit('/favs')
+        cy.get('form').should('have.length', 2)
+
+    })
+})
